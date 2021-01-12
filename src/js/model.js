3b9c4da65a820440d6d0ebdb38a6c7e4aa7568c0
SharkyBlue <MiGSnow81@gmail.com>
2021-01-13 00:04:17 +0100

    Edited version1

--- src/js/model.js
+++ src/js/model.js
@@ -120,7 +120,6 @@ export const uploadRecipe = async function (newRecipe) {
       .filter(entry => entry[0].startsWith('ingredient') && entry[1] !== '')
       .map(ing => {
         const ingArr = ing[1].split(',').map(el => el.trim());
-        // const ingArr = ing[1].replaceAll(' ', '').split(',');
         if (ingArr.length !== 3)
           throw new Error(
             'Wrong ingredient format! Please use the correct format'
